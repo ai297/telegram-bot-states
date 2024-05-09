30be1aaeee76185f94d38fd2c233acdd7b09d5e4
AI.297 <sw.darkwind@gmail.com>
2024-05-09 19:14:29 +0300

    Update menu button setup logic - setup commands even menu button is configured

--- src/Telegram.Bot.States/State/StateSetupService.cs
+++ src/Telegram.Bot.States/State/StateSetupService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
 namespace Telegram.Bot.States;
 
@@ -21,9 +20,6 @@ internal class StateSetupService(ITelegramBotClient botClient,
         var menuButton = getMenuButton != null
             ? getMenuButton(update.User.LanguageCode ?? defaultLanguageCode)
             : new MenuButtonDefault();
-
-        if (menuButton.Type == MenuButtonType.WebApp)
-            return botClient.SetChatMenuButtonAsync(update.User.Id, menuButton);
 
         var scope = BotCommandScope.Chat(chatState.ChatId);
         var hasMultiLanguageSupport = allLanguageCodes.Count > 1;
